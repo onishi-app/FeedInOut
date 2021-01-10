72bc1b8ef13bb74d4a0bd5ea70258c7922a1113d
Onishi Ryosuke <onishi.appcreate@gmail.com>
2021-01-10 13:58:56 +0900

    UI実装

--- FeedInOut/ViewController.swift
+++ FeedInOut/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        button.layer.cornerRadius = 15
+        
+    }
+    
+    // ボタンが押された時の処理
+    @IBAction func buttonAction(_ sender: Any) {
+        print("ボタンがタップされました。")
     }
 }
 
